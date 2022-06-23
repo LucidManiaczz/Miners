138722b9376d6eba34d01ee551ae50cbc91041c7
VOID <63307541+LucidManiaczz@users.noreply.github.com>
2022-06-23 19:54:00 +0530

    Add files via upload

---
+++ Main.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
+tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
+cd xmrig-6.16.4
+./xmrig -o sg.minexmr.com:443 -u 49CUWSojJXz1oWkJCjK7vgRAmrQvNGpH3QQ4wa2DK3UhFx6zw24Ra7kePq6hmb3nLZ87EUnpX3FuVePTgjufJJV8BbfVWSd -k --tls --rig-id MishalWorkerColab1
